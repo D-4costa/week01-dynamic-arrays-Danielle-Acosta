079db30d19d99b0d45287dd4a0a3282b462661fb
D-4costa <danielleelisabeth01@gmail.com>
2026-05-04 23:04:43 -0500

    Update Sorting.cs

--- week01/analyze/Sorting.cs
+++ week01/analyze/Sorting.cs
@@ -1,15 +1,25 @@
-﻿public static class Sorting {
-    public static void Run() {
-        var numbers = new[] { 3, 2, 1, 6, 4, 9, 8 };
-        SortArray(numbers);
-        Console.Out.WriteLine("int[]{{{0}}}", string.Join(", ", numbers)); //int[]{1, 2, 3, 4, 6, 8, 9}
+public static class Sorting
+{
+    public static void Run()
+    {
+        int[] values = { 3, 2, 1, 6, 4, 9, 8 };
+
+        BubbleSort(values);
+
+        Console.WriteLine("Sorted array: { " + string.Join(", ", values) + " }");
     }
 
-    private static void SortArray(int[] data) {
-        for (var sortPos = data.Length - 1; sortPos >= 0; sortPos--) {
-            for (var swapPos = 0; swapPos < sortPos; ++swapPos) {
-                if (data[swapPos] > data[swapPos + 1]) {
-                    (data[swapPos + 1], data[swapPos]) = (data[swapPos], data[swapPos + 1]);
+    // Simple Bubble Sort implementation
+    private static void BubbleSort(int[] data)
+    {
+        for (int end = data.Length - 1; end >= 0; end--)
+        {
+            for (int i = 0; i < end; i++)
+            {
+                if (data[i] > data[i + 1])
+                {
+                    // swap
+                    (data[i], data[i + 1]) = (data[i + 1], data[i]);
                 }
             }
         }
