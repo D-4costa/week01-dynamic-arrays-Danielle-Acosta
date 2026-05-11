00ce55b1d0f8bd29be2362f1585719a07e910f13
D-4costa <danielleelisabeth01@gmail.com>
2026-05-11 12:54:27 -0500

    Update Program.cs

--- week02/teach/Program.cs
+++ week02/teach/Program.cs
@@ -1,3 +1,4 @@
-﻿Console.WriteLine("\n======================\nCustomer Service\n======================");
+Console.WriteLine("\n======================\nCustomer Service\n======================");
 CustomerService.Run();
+// Uncomment this line if you want to run the solution version
 // CustomerServiceSolution.Run();
