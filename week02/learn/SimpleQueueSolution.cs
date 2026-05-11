6f35089a673e291a5f61795af750f954b53eb372
D-4costa <danielleelisabeth01@gmail.com>
2026-05-11 12:51:04 -0500

    Update SimpleQueueSolution.cs

--- week02/learn/SimpleQueueSolution.cs
+++ week02/learn/SimpleQueueSolution.cs
@@ -1,16 +1,24 @@
-﻿public class SimpleQueueSolution {
-    public static void Run() {
+public class SimpleQueueSolution
+{
+    public static void Run()
+    {
         // Test Cases
 
         // Test 1
         // Scenario: Enqueue one value and then Dequeue it.
         // Expected Result: It should display 100
         Console.WriteLine("Test 1");
+
         var queue = new SimpleQueueSolution();
+
         queue.Enqueue(100);
+
         var value = queue.Dequeue();
+
         Console.WriteLine(value);
-        // Defect(s) Found: Trying to remove data from index 1 rather than 0
+
+        // Defect(s) Found:
+        // The dequeue method was removing data from index 1 instead of index 0.
 
         Console.WriteLine("------------");
 
@@ -18,17 +26,24 @@
         // Scenario: Enqueue multiple values and then Dequeue all of them
         // Expected Result: It should display 200, then 300, then 400 in that order
         Console.WriteLine("Test 2");
+
         queue = new SimpleQueueSolution();
+
         queue.Enqueue(200);
         queue.Enqueue(300);
         queue.Enqueue(400);
+
         value = queue.Dequeue();
         Console.WriteLine(value);
+
         value = queue.Dequeue();
         Console.WriteLine(value);
+
         value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found: The enqueue was inserting at the front of the queue
+
+        // Defect(s) Found:
+        // The enqueue method was adding items to the front instead of the back of the queue.
 
         Console.WriteLine("------------");
 
@@ -36,15 +51,21 @@
         // Scenario: Dequeue from an empty Queue
         // Expected Result: An exception should be raised
         Console.WriteLine("Test 3");
+
         queue = new SimpleQueueSolution();
-        try {
+
+        try
+        {
             queue.Dequeue();
             Console.WriteLine("Oops ... This shouldn't have worked.");
         }
-        catch (IndexOutOfRangeException) {
+        catch (IndexOutOfRangeException)
+        {
             Console.WriteLine("I got the exception as expected.");
         }
-        // Defect(s) Found: None :)
+
+        // Defect(s) Found:
+        // No additional defects were found after fixing the queue.
     }
 
     private readonly List<int> _queue = new();
@@ -53,8 +74,9 @@
     /// Enqueue the value provided into the queue
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
-    private void Enqueue(int value) {
-        _queue.Add(value); // Defect 2 - Need to add to the end of the queue
+    private void Enqueue(int value)
+    {
+        _queue.Add(value);
     }
 
     /// <summary>
@@ -62,12 +84,17 @@
     /// </summary>
     /// <exception cref="IndexOutOfRangeException">If queue is empty</exception>
     /// <returns>First integer in the queue</returns>
-    private int Dequeue() {
+    private int Dequeue()
+    {
         if (_queue.Count <= 0)
+        {
             throw new IndexOutOfRangeException();
+        }
 
-        var value = _queue[0]; // Defect 1 - lists start at index 0
+        var value = _queue[0];
+
         _queue.RemoveAt(0);
+
         return value;
     }
 }
