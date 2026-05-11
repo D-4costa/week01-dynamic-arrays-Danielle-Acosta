c1d57c491bbded53182b301883ec38fed114377a
D-4costa <danielleelisabeth01@gmail.com>
2026-05-11 12:53:03 -0500

    Refactor ComplexStack to improve readability

--- week02/teach/ComplexStack.cs
+++ week02/teach/ComplexStack.cs
@@ -1,24 +1,46 @@
-﻿public static class ComplexStack {
-    public static bool DoSomethingComplicated(string line) {
+public static class ComplexStack
+{
+    public static bool DoSomethingComplicated(string line)
+    {
         var stack = new Stack<char>();
-        foreach (var item in line) {
-            if (item is '(' or '[' or '{') {
+
+        foreach (var item in line)
+        {
+            // Save opening symbols
+            if (item is '(' or '[' or '{')
+            {
                 stack.Push(item);
             }
-            else if (item is ')') {
+
+            // Check closing parenthesis
+            else if (item == ')')
+            {
                 if (stack.Count == 0 || stack.Pop() != '(')
+                {
                     return false;
+                }
             }
-            else if (item is ']') {
+
+            // Check closing bracket
+            else if (item == ']')
+            {
                 if (stack.Count == 0 || stack.Pop() != '[')
+                {
                     return false;
+                }
             }
-            else if (item is '}') {
+
+            // Check closing brace
+            else if (item == '}')
+            {
                 if (stack.Count == 0 || stack.Pop() != '{')
+                {
                     return false;
+                }
             }
         }
 
+        // Stack should be empty if everything matched correctly
         return stack.Count == 0;
     }
 }
