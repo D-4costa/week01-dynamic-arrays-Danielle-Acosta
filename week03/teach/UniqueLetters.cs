c297f285a135da80b6eccdb0187e6d1fc7c2753f
D-4costa <danielleelisabeth01@gmail.com>
2026-05-18 11:58:24 -0500

    Optimize AreUniqueLetters method for duplicate check
    Refactor AreUniqueLetters method to use HashSet for O(n) efficiency.

--- week03/teach/UniqueLetters.cs
+++ week03/teach/UniqueLetters.cs
@@ -1,26 +1,32 @@
-﻿public static class UniqueLetters {
-    public static void Run() {
-        var test1 = "abcdefghjiklmnopqrstuvwxyz"; // Expect True because all letters unique
+public static class UniqueLetters
+{
+    public static void Run()
+    {
+        var test1 = "abcdefghjiklmnopqrstuvwxyz";
         Console.WriteLine(AreUniqueLetters(test1));
 
-        var test2 = "abcdefghjiklanopqrstuvwxyz"; // Expect False because 'a' is repeated
+        var test2 = "abcdefghjiklanopqrstuvwxyz";
         Console.WriteLine(AreUniqueLetters(test2));
 
         var test3 = "";
-        Console.WriteLine(AreUniqueLetters(test3)); // Expect True because its an empty string
+        Console.WriteLine(AreUniqueLetters(test3));
     }
 
-    /// <summary>Determine if there are any duplicate letters in the text provided</summary>
-    /// <param name="text">Text to check for duplicate letters</param>
-    /// <returns>true if all letters are unique, otherwise false</returns>
-    private static bool AreUniqueLetters(string text) {
-        // TODO Problem 1 - Replace the O(n^2) algorithm to use sets and O(n) efficiency
-        for (var i = 0; i < text.Length; ++i) {
-            for (var j = 0; j < text.Length; ++j) {
-                // Don't want to compare to yourself ... that will always result in a match
-                if (i != j && text[i] == text[j])
-                    return false;
+    /// <summary>
+    /// Determine if there are duplicate letters
+    /// </summary>
+    private static bool AreUniqueLetters(string text)
+    {
+        HashSet<char> lettersFound = new();
+
+        foreach (char currentLetter in text)
+        {
+            if (lettersFound.Contains(currentLetter))
+            {
+                return false;
             }
+
+            lettersFound.Add(currentLetter);
         }
 
         return true;
