b02eb80c8aa183dba93abc303f78f4984c35a2da
D-4costa <danielleelisabeth01@gmail.com>
2026-05-19 14:49:08 -0500

    Refactor UniqueLetters class for clarity and efficiency

--- week03/teach/UniqueLetters.cs
+++ week03/teach/UniqueLetters.cs
@@ -1,32 +1,28 @@
-public static class UniqueLetters
-{
-    public static void Run()
-    {
-        var test1 = "abcdefghjiklmnopqrstuvwxyz";
+public static class UniqueLetters {
+    public static void Run() {
+        var test1 = "abcdefghjiklmnopqrstuvwxyz"; // Expect True because all letters unique
         Console.WriteLine(AreUniqueLetters(test1));
 
-        var test2 = "abcdefghjiklanopqrstuvwxyz";
+        var test2 = "abcdefghjiklanopqrstuvwxyz"; // Expect False because 'a' is repeated
         Console.WriteLine(AreUniqueLetters(test2));
 
         var test3 = "";
-        Console.WriteLine(AreUniqueLetters(test3));
+        Console.WriteLine(AreUniqueLetters(test3)); // Expect True because its an empty string
     }
 
-    /// <summary>
-    /// Determine if there are duplicate letters
-    /// </summary>
-    private static bool AreUniqueLetters(string text)
-    {
-        HashSet<char> lettersFound = new();
+    /// <summary>Determine if there are any duplicate letters in the text provided</summary>
+    /// <param name="text">Text to check for duplicate letters</param>
+    /// <returns>true if all letters are unique, otherwise false</returns>
+    private static bool AreUniqueLetters(string text) {
 
-        foreach (char currentLetter in text)
+        var found = new HashSet<char>();
+
+        foreach (var letter in text)
         {
-            if (lettersFound.Contains(currentLetter))
-            {
+            if (found.Contains(letter))
                 return false;
-            }
 
-            lettersFound.Add(currentLetter);
+            found.Add(letter);
         }
 
         return true;
