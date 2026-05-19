4027e76afacd209096d1b1a0f64012e90222f953
D-4costa <danielleelisabeth01@gmail.com>
2026-05-19 14:41:23 -0500

    }
    }

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -14,17 +14,11 @@ public class Translator
 
     private Dictionary<string, string> _words = new();
 
-    /// <summary>
-    /// Add the translation from 'from_word' to 'to_word'
-    /// </summary>
     public void AddWord(string fromWord, string toWord)
     {
         _words[fromWord] = toWord;
     }
 
-    /// <summary>
-    /// Translates the from word into the stored translation
-    /// </summary>
     public string Translate(string fromWord)
     {
         if (_words.ContainsKey(fromWord))
